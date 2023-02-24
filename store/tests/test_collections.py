f1935ad2406ba92034842683a6e6d02fff6a7ad4
MohamadrezaPiri <piry.mreza@gmail.com>
2023-02-24 13:34:09 +0330

    Write test for updating a collection

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -2,6 +2,7 @@ import pytest
 from model_bakery import baker
 from rest_framework import status
 from user.models import User
+from store.models import Collection
 
 
 @pytest.fixture
@@ -9,6 +10,13 @@ def create_collection(api_client):
     def do_create_collection(collection):
         return api_client.post('/collections/', collection)
     return do_create_collection
+
+
+@pytest.fixture
+def update_collection(api_client):
+    def do_update_collection(collection_id, collection):
+        return api_client.put(f'/collections/{collection_id}/', collection)
+    return do_update_collection
 
 
 @pytest.mark.django_db
@@ -42,3 +50,40 @@ class TestCreateCollection:
         response = create_collection({"title": 'a'})
 
         assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+class TestUpdateCollection:
+    def test_if_user_is_anonymous_returns_401(self, update_collection):
+        collection = baker.make(Collection)
+
+        response = update_collection(collection.id, {"title": 'a'})
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self, update_collection, authenticate):
+        collection = baker.make(Collection)
+        user = baker.make(User)
+
+        authenticate(user=user)
+        response = update_collection(collection.id, {"title": 'a'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_not_valid_returns_400(self, update_collection, authenticate):
+        collection = baker.make(Collection)
+        admin = baker.make(User, is_staff=True)
+
+        authenticate(user=admin)
+        response = update_collection(collection.id, {"title": ''})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_if_data_is_valid_returns_200(self, update_collection, authenticate):
+        collection = baker.make(Collection)
+        admin = baker.make(User, is_staff=True)
+
+        authenticate(user=admin)
+        response = update_collection(collection.id, {"title": 'a'})
+
+        assert response.status_code == status.HTTP_200_OK
