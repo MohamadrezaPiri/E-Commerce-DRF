47ae8fb5e7336af928bdb53155a6cd0ff34945a6
MohamadrezaPiri <piry.mreza@gmail.com>
2023-03-01 13:05:15 +0330

    Wrote test for deleting a cart

--- store/tests/test_carts.py
+++ store/tests/test_carts.py
@@ -1,5 +1,7 @@
 import pytest
+from model_bakery import baker
 from rest_framework import status
+from store.models import Cart
 
 
 @pytest.mark.django_db
@@ -9,3 +11,12 @@ class TestCreateCart:
         response = api_client.post('/carts/')
 
         assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+class TestDeleteCart:
+    def test_if_delete_request_is_sent_returns_204(self, api_client):
+        cart = baker.make(Cart)
+        response = api_client.delete(f'/carts/{cart.id}/')
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT
