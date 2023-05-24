1a3c5b78f49630a71a86d6e62304e4dedd410ffb
MohamadrezaPiri <piry.mreza@gmail.com>
2023-05-24 13:00:00 +0430

    Create ProductsCountFilter in filters.py

--- store/filters.py
+++ store/filters.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import FilterSet
 from django.db.models.query import QuerySet
+from django.db.models import Count
 from django.contrib import admin
 from .models import Product
 
@@ -25,3 +26,22 @@ class InventoryFilter(admin.SimpleListFilter):
     def queryset(self, request, queryset: QuerySet):
         if self.value() == '<10':
             return queryset.filter(inventory__lt=10)
+
+
+class ProductsCountFilter(admin.SimpleListFilter):
+    title = 'Products count'
+    parameter_name = 'Products_count'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('0<', 'With product'),
+            ('<1', 'Without product')
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        annotated_value = queryset.annotate(products_count=Count('products'))
+
+        if self.value() == '0<':
+            return annotated_value.filter(products_count__gt=0)
+        elif self.value() == '<1':
+            return annotated_value.filter(products_count__lt=1)
