81d81df878c8544f35ccaa0a5d2d5cfc1203aecd
MohamadrezaPiri <piry.mreza@gmail.com>
2023-05-25 11:46:35 +0430

    Add id & user to list_display in CustomerAdmin class

--- store/admin.py
+++ store/admin.py
@@ -76,7 +76,8 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name',  'membership', 'orders']
+    list_display = ['id', 'user', 'first_name',
+                    'last_name',  'membership', 'orders']
     list_select_related = ['user']
     list_editable = ['membership']
     list_per_page = 10
