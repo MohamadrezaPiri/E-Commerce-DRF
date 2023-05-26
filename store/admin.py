52a6d307981ddcccf8e537abf3564a03bdc91b1c
MohamadrezaPiri <piry.mreza@gmail.com>
2023-05-26 15:25:29 +0430

    Change search_fields in CustomerAdmin class

--- store/admin.py
+++ store/admin.py
@@ -83,7 +83,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_filter = ['membership', OrdersCountFilter]
     list_per_page = 10
     ordering = ['user__first_name', 'user__last_name']
-    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+    search_fields = ['user__username']
 
     @admin.display(ordering='orders_count')
     def orders(self, customer):
