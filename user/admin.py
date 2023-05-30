55c0db3c1c0dddbfe7d7690b885a4ad48c2624b3
MohamadrezaPiri <piry.mreza@gmail.com>
2023-05-30 11:34:39 +0330

    Add fields to UserAdmin class

--- user/admin.py
+++ user/admin.py
@@ -8,4 +8,6 @@ class UserAdmin(admin.ModelAdmin):
     list_editable = ['is_staff']
     list_filter = ['is_staff']
     list_per_page = 10
+    fields = ['username', 'first_name', 'last_name',
+              'email', 'password', 'is_staff']
     search_fields = ['username']
