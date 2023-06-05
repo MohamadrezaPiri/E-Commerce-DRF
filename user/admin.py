fc450be014e33851c45077d52b72da324c41b448
MohamadrezaPiri <piry.mreza@gmail.com>
2023-06-05 21:57:07 +0330

    Define get_queryset method in UserAdmin class

--- user/admin.py
+++ user/admin.py
@@ -1,13 +1,22 @@
+from django.utils.html import format_html, urlencode
+from django.db.models import Count
 from django.contrib import admin
+from django.urls import reverse
 from .models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'first_name', 'last_name', 'email', 'is_staff']
+    list_display = ['username', 'first_name',
+                    'last_name', 'email', 'is_staff']
     list_editable = ['is_staff']
     list_filter = ['is_staff']
     list_per_page = 10
     fields = ['username', 'first_name', 'last_name',
               'email', 'password', 'is_staff']
     search_fields = ['username']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            reviews_count=Count('reviews')
+        )
